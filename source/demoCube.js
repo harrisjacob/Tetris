bf0b84906450fc801ad41160307ebd8e444b0cb9
haleyholden <hholden@nd.edu>
2021-04-27 18:28:31 -0400

    Modularized ladA

--- source/demoCube.js
+++ source/demoCube.js
@@ -6,7 +6,8 @@ import {
 
 let scene, camera, renderer;
 let speed = 1;
-var cube;
+var rotateCount = 0;
+let ladA;
 
 window.onload = function init() {
 
@@ -53,20 +54,45 @@ window.onload = function init() {
     const grid = new THREE.GridHelper(50, 50, 0x888888, 0x888888);
     scene.add(grid);
 
+    //
+    //    //Box Geometry
+    //    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    //    const material = new THREE.MeshPhongMaterial()
+    //    material.color = new THREE.Color(0xff0000);
+    //
+    //    const material1 = new THREE.MeshPhongMaterial()
+    //    material1.color = new THREE.Color(0x100022);
+    //
+    //    cubeA = new THREE.Mesh(geometry, material1);
+    //    cubeA.castShadow = true;
+    //    cubeA.position.x = 0;
+    //    cubeA.position.y = 6;
+    //    cubeA.position.z = 0;
+    //
+    //    cubeB = new THREE.Mesh(geometry, material);
+    //    cubeB.castShadow = true;
+    //    cubeB.position.x = 0;
+    //    cubeB.position.y = 7;
+    //    cubeB.position.z = 0;
+    //
+    //    cubeC = new THREE.Mesh(geometry, material);
+    //    cubeC.castShadow = true;
+    //    cubeC.position.x = 0;
+    //    cubeC.position.y = 8;
+    //    cubeC.position.z = 0;
+    //
+    //    cubeD = new THREE.Mesh(geometry, material);
+    //    cubeD.castShadow = true;
+    //    cubeD.position.x = 1;
+    //    cubeD.position.y = 7;
+    //    cubeD.position.z = 0;
+    //
+    //    scene.add(cubeA);
+    //    scene.add(cubeB);
+    //    scene.add(cubeC);
+    //    scene.add(cubeD);
 
-    //Box Geometry
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshPhongMaterial()
-    material.color = new THREE.Color(0xff0000);
-    cube = new THREE.Mesh(geometry, material);
-    cube.castShadow = true;
-    cube.position.x = 0;
-    cube.position.y = 6;
-    cube.position.z = 0;
-
-    scene.add(cube);
-
-
+    ladA = new LadA(scene);
 
 
     //Renderer
@@ -83,20 +109,17 @@ window.onload = function init() {
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.target.set(0, 0.1, 0);
     controls.update();
-    //controls.minDistance = 0.5;
-    //controls.maxDistance = 10;
     controls.maxPolarAngle = 0.5 * Math.PI;
 
     //Buttons
     document.getElementById("plus").onclick = function () {
-        console.log(speed);
-        speed += 0.01;
+        ladA.speed += 0.01;
     };
 
     document.getElementById("minus").onclick = function () {
         console.log(speed);
         if (speed > 0.0001) {
-            speed -= 0.01;
+            ladA.speed -= 0.01;
         }
     };
 
@@ -109,10 +132,7 @@ window.onload = function init() {
 }
 
 const animate = function () {
-
-    if (cube.position.y > 0.5) {
-        cube.position.y -= speed * 0.01;
-    }
+    ladA.fall();
     requestAnimationFrame(animate)
     renderer.render(scene, camera);
 };
@@ -121,13 +141,35 @@ function checkKey(e) {
 
     e = e || window.event;
 
-    if (e.keyCode == '37') {
-        cube.position.x -= 1;
-    } else if (e.keyCode == '39') {
-        cube.position.x += 1;
+    if (e.keyCode == '37') { // left arrow
+        ladA.moveLeft();
+    } else if (e.keyCode == '39') { // right arrow
+        ladA.moveRight();
+    } else if (e.keyCode == '32') { //space bar
+
+        rotateCount += 1;
+        //var pp, var pm, var mp, var mm
+        switch (rotateCount) {
+            case 1:
+                ladA.rotate1();
+                break;
+            case 2:
+                ladA.rotate2();
+                break;
+            case 3:
+                ladA.rotate3();
+                break;
+            case 4:
+                ladA.rotate4();
+                rotateCount = 0;
+                break;
+            default:
+                break;
+        }
     }
 
 }
+
 
 
 function onWindowResize() {
@@ -136,5 +178,128 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+class LadA {
+    constructor(scene) {
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshPhongMaterial()
+        material.color = new THREE.Color(0xff0000);
+
+
+        this.cubeA = new THREE.Mesh(geometry, material);
+        this.cubeA.castShadow = true;
+        this.cubeA.position.x = 0;
+        this.cubeA.position.y = 6;
+        this.cubeA.position.z = 0;
+
+        this.cubeB = new THREE.Mesh(geometry, material);
+        this.cubeB.castShadow = true;
+        this.cubeB.position.x = 0;
+        this.cubeB.position.y = 7;
+        this.cubeB.position.z = 0;
+
+        this.cubeC = new THREE.Mesh(geometry, material);
+        this.cubeC.castShadow = true;
+        this.cubeC.position.x = 0;
+        this.cubeC.position.y = 8;
+        this.cubeC.position.z = 0;
+
+        this.cubeD = new THREE.Mesh(geometry, material);
+        this.cubeD.castShadow = true;
+        this.cubeD.position.x = 1;
+        this.cubeD.position.y = 7;
+        this.cubeD.position.z = 0;
+
+        this.speed = 0.01;
+
+        scene.add(this.cubeA);
+        scene.add(this.cubeB);
+        scene.add(this.cubeC);
+        scene.add(this.cubeD);
+
+    }
+
+    fall() {
+
+        if (this.getMinY() > 0.5) {
+            this.cubeA.position.y -= this.speed;
+            this.cubeB.position.y -= this.speed;
+            this.cubeC.position.y -= this.speed;
+            this.cubeD.position.y -= this.speed;
+        }
+
+    }
+
+
+    rotate1() {
+        this.cubeA.position.x -= 1;
+        this.cubeA.position.y += 1;
+
+        this.cubeC.position.x += 1;
+        this.cubeC.position.y -= 1;
+
+        this.cubeD.position.x -= 1;
+        this.cubeD.position.y -= 1;
+
+    }
+
+    rotate2() {
+        this.cubeA.position.x += 1;
+        this.cubeA.position.y += 1;
+
+        this.cubeD.position.x -= 1;
+        this.cubeD.position.y += 1;
+
+        this.cubeC.position.x -= 1;
+        this.cubeC.position.y -= 1;
+
+    }
+
+    rotate3() {
+        this.cubeA.position.x += 1;
+        this.cubeA.position.y -= 1;
+
+        this.cubeC.position.x -= 1;
+        this.cubeC.position.y += 1;
+
+        this.cubeD.position.x += 1;
+        this.cubeD.position.y += 1;
+
+    }
+
+    rotate4() {
+        this.cubeA.position.x -= 1;
+        this.cubeA.position.y -= 1;
+
+        this.cubeC.position.x += 1;
+        this.cubeC.position.y += 1;
+
+        this.cubeD.position.x += 1;
+        this.cubeD.position.y -= 1;
+    }
+
+    moveLeft() {
+        this.cubeA.position.x -= 1;
+        this.cubeB.position.x -= 1;
+        this.cubeC.position.x -= 1;
+        this.cubeD.position.x -= 1;
+    }
+
+    moveRight() {
+        this.cubeA.position.x += 1;
+        this.cubeB.position.x += 1;
+        this.cubeC.position.x += 1;
+        this.cubeD.position.x += 1;
+    }
+
+    getMinY() {
+
+        return Math.min(this.cubeA.position.y, this.cubeB.position.y, this.cubeC.position.y, this.cubeD.position.y)
+    }
+
+
+
 
 }

ddcf7ad3b333fc5641d59c37fb4daf7cd120b706
haleyholden <hholden@nd.edu>
2021-04-27 15:11:28 -0400

    Added two buttons

--- source/demoCube.js
+++ source/demoCube.js
@@ -1,82 +1,105 @@
 import * as THREE from '../build/three.module.js';
 
-import { OrbitControls } from './jsm/controls/OrbitControls.js';
+import {
+    OrbitControls
+} from './jsm/controls/OrbitControls.js';
 
 let scene, camera, renderer;
+let speed = 1;
 
 window.onload = function init() {
 
 
-	const container = document.getElementById( 'container' );
+    const container = document.getElementById('container');
 
-	//Set up camera
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 100 );
-	camera.position.set( 3, 2, 16 );
+    //Set up camera
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
+    camera.position.set(3, 2, 16);
 
-	//Set up Scene
-	scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0xa0a0a0 );
-	//scene.fog = new THREE.Fog( 0xa0a0a0, 2, 20 );
-
-
-	//Lighting
-	const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-	hemiLight.position.set( 0, 20, 0 );
-	scene.add( hemiLight );
-
-	const dirLight = new THREE.DirectionalLight( 0xffffff );
-	dirLight.position.set( 5, 5, 0 );
-	dirLight.castShadow = true;
-	dirLight.shadow.camera.top = 1;
-	dirLight.shadow.camera.bottom = - 1;
-	dirLight.shadow.camera.left = - 1;
-	dirLight.shadow.camera.right = 1;
-	dirLight.shadow.camera.near = 0.1;
-	dirLight.shadow.camera.far = 20;
-	scene.add( dirLight );
+    //Set up Scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xa0a0a0);
+    //scene.fog = new THREE.Fog( 0xa0a0a0, 2, 20 );
 
 
-	//Geometry
-	const geometry = new THREE.BoxGeometry();
-	const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-	const cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
+    //Lighting
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+    hemiLight.position.set(0, 20, 0);
+    scene.add(hemiLight);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff);
+    dirLight.position.set(5, 5, 0);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 1;
+    dirLight.shadow.camera.bottom = -1;
+    dirLight.shadow.camera.left = -1;
+    dirLight.shadow.camera.right = 1;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 20;
+    scene.add(dirLight);
 
 
-	//Renderer
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.outputEncoding = THREE.sRGBEncoding;
-	renderer.shadowMap.enabled = true;
-	container.appendChild( renderer.domElement );
-
-	//Trackball
-	const controls = new OrbitControls( camera, renderer.domElement );
-	controls.target.set( 0, 0.1, 0 );
-	controls.update();
-	controls.minDistance = 0.5;
-	controls.maxDistance = 10;
-	controls.maxPolarAngle = 0.5 * Math.PI;
+    //Geometry
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({
+        color: 0xffffff
+    });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
 
 
-	window.addEventListener( 'resize', onWindowResize );
+    //Renderer
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.shadowMap.enabled = true;
+    container.appendChild(renderer.domElement);
 
-	animate();
+    //Trackball
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0.1, 0);
+    controls.update();
+    controls.minDistance = 0.5;
+    controls.maxDistance = 10;
+    controls.maxPolarAngle = 0.5 * Math.PI;
+
+    //Buttons
+    document.getElementById("plus").onclick = function () {
+        console.log("increase speed");
+        speed += 1;
+    };
+
+    document.getElementById("minus").onclick = function () {
+        console.log("decrease speed");
+        if (speed > 1) {
+            speed -= 1;
+        }
+    };
+
+
+
+
+
+    window.addEventListener('resize', onWindowResize);
+
+    animate();
 }
 
 const animate = function () {
 
-	requestAnimationFrame( animate )
-	renderer.render( scene, camera );
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera);
 };
 
 function onWindowResize() {
 
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
 }

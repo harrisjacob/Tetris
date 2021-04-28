fc6570453cbe99fe304e6fb6cde73285a6e08586
Bella Murrer <imurrer@nd.edu>
2021-04-27 23:08:03 -0400

    breaking rows and moving above blocks down

--- source/demoCube.js
+++ source/demoCube.js
@@ -8,6 +8,23 @@ let scene, camera, renderer;
 let speed = 1;
 var cube;
 
+var board_positions = [];
+var broken_rows;
+
+function createBlock(){
+    //Box Geometry
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshPhongMaterial()
+    material.color = new THREE.Color(0xff0000);
+    cube = new THREE.Mesh(geometry, material);
+    cube.castShadow = true;
+    cube.position.x = 4;
+    cube.position.y = 6.5;
+    cube.position.z = 0;
+
+    scene.add(cube);
+}
+
 window.onload = function init() {
 
 
@@ -54,17 +71,8 @@ window.onload = function init() {
     scene.add(grid);
 
 
-    //Box Geometry
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshPhongMaterial()
-    material.color = new THREE.Color(0xff0000);
-    cube = new THREE.Mesh(geometry, material);
-    cube.castShadow = true;
-    cube.position.x = 0;
-    cube.position.y = 6;
-    cube.position.z = 0;
-
-    scene.add(cube);
+    // Create Block
+    createBlock();
 
 
 
@@ -100,31 +108,103 @@ window.onload = function init() {
         }
     };
 
+    // Initialize board positions
+    board_positions = (new Array(20)).fill().map(function(){ return new Array(10).fill(0);});
+
     //Arrow Keys
     document.onkeydown = checkKey;
+    // window.addEventListener('click', doRender);
 
     window.addEventListener('resize', onWindowResize);
 
     animate();
 }
 
+// Check if any row is full
+function breakRows(){
+    broken_rows = 0;
+    for(var i=0; i<board_positions.length; i++){
+        if(!board_positions[i].includes(0)){
+            broken_rows++;
+            // Delete full row and add empty row at the top
+            for(var j=0; j<board_positions[i].length; j++){
+                var to_delete = board_positions[i][j]
+                scene.remove(to_delete);
+                to_delete.geometry.dispose();
+                to_delete.material.dispose();
+            }
+            board_positions.splice(i, 1);
+            // TODO: Add to the score for the broken row
+            // TODO: function call to delete the ith row of boxese visually from the scene
+            board_positions.unshift(new Array(10).fill(0));
+        }
+    }
+    if(broken_rows>0){
+        for(var i=0; i<board_positions.length; i++){
+            for(var j=0; j<board_positions[i].length; j++){
+                if(board_positions[i][j] != 0){
+                    board_positions[i][j].position.y -= broken_rows;
+                }
+            }
+        }
+    }
+}
+
 const animate = function () {
 
+    var col = cube.position.x;
+    var row = 19 - (cube.position.y - 0.5);
+    
     if (cube.position.y > 0.5) {
-        cube.position.y -= speed * 0.01;
+        if(row < 19){
+            // Stop the block from falling if it should be stacked
+            if(board_positions[row+1][col] != 0){
+                board_positions[row][col] = cube;
+                breakRows();
+                createBlock();
+            } else {
+                cube.position.y -= 1; 
+            }
+        } else {
+            
+            cube.position.y -= 1; 
+        }
+        // cube.position.y -= speed * 0.01;
     }
-    requestAnimationFrame(animate)
+    else{
+        // 2D position matrix starts with row 0 at the top, but real cube y position has 0 at plane level
+        // So, subtract 19 to convert
+        board_positions[row][col] = cube;
+        console.log(board_positions);
+        breakRows();
+        console.log(broken_rows);
+        createBlock();
+    }
+
+    // Pause between renders to make the boxes drop incrementally
+    setTimeout(function(){
+        requestAnimationFrame(animate);
+    }, 500);
     renderer.render(scene, camera);
+    
 };
 
 function checkKey(e) {
 
     e = e || window.event;
-
-    if (e.keyCode == '37') {
-        cube.position.x -= 1;
-    } else if (e.keyCode == '39') {
-        cube.position.x += 1;
+    var col = cube.position.x;
+    var row = 19 - (cube.position.y - 0.5);
+    // Only move left or right if within the 10 block space, above the ground plane, and there are no blocks next to it
+    if (e.keyCode == '37' && cube.position.x >= 1 && cube.position.y > 0.5) {
+        if(board_positions[row][col-1]==0){
+            cube.position.x -= 1;
+            renderer.render(scene, camera);
+        }
+    } else if (e.keyCode == '39' && cube.position.x < 9 && cube.position.y > 0.5 && board_positions[row][col]==0) {
+        if(board_positions[row][col+1]==0){
+            cube.position.x += 1;
+            renderer.render(scene, camera);
+        }
     }
 
 }

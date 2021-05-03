ec6005855281eec9713a493eba2876677097e0fc
Jacob Harris <harrisjakef@gmail.com>
2021-05-02 20:27:01 -0400

    Adjusted Piece T rotations

--- source/components/PieceT.js
+++ source/components/PieceT.js
@@ -8,17 +8,17 @@ export default class PieceT extends BasicPiece{
     constructor(scene) {  
         super();
         
-        this.cubeA = new Block(scene, 0,6,0);
+        this.cubeA = new Block(scene, -1,7,0);
         this.cubeB = new Block(scene, 0,7,0);
-        this.cubeC = new Block(scene, 0,8,0);
-        this.cubeD = new Block(scene, 1,7,0);        
+        this.cubeC = new Block(scene, 1,7,0);
+        this.cubeD = new Block(scene, 0,8,0);        
 
         this.rotations = {
             A : [
-                    { x: -1, y: 1},
                     { x: 1, y: 1},
                     { x: 1, y: -1},
                     { x: -1, y: -1},
+                    { x: -1, y: 1},
                 ],
             B : [
                     { x: 0, y: 0},
@@ -27,16 +27,16 @@ export default class PieceT extends BasicPiece{
                     { x: 0, y: 0},
                 ],
             C : [
-                    { x: 1, y: -1},
                     { x: -1, y: -1},
                     { x: -1, y: 1},
                     { x: 1, y: 1},
+                    { x: 1, y: -1},
                 ],
             D : [
+                    { x: 1, y: -1},
                     { x: -1, y: -1},
                     { x: -1, y: 1},
                     { x: 1, y: 1},
-                    { x: 1, y: -1},
                 ],
         };
 
